595223860c130ade5eda252ac5a280210d4a4bae
Thang692004 <tangledinhthang0609@gmail.com>
2026-07-18 01:20:38 +0700

    commit

--- lib/services/realtime_dispatcher.dart
+++ lib/services/realtime_dispatcher.dart
@@ -29,7 +29,12 @@ class RealtimeDispatcher {
       case "het_cho":
         swipeCardProvider.showStatusMessage("Đã hết chỗ để xe");
         break;
-
+      case "quet_roi":
+        swipeCardProvider.showStatusMessage("Thẻ ${message.data['cid']} không hợp lệ hoặc đã được quẹt ra trước đó");
+        break;
+      case "khong_hop_le":
+        swipeCardProvider.showStatusMessage("Thẻ không hợp lệ");
+        break;
       default:
         debugPrint("Unknown message: ${message.type}");
     }
